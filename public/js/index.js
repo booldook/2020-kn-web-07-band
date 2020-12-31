d175864b0174067f63dd183b050c3fb5ead24283
booldook <booldook@gmail.com>
2020-12-31 12:27:06 +0900

    band-wrapper

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,22 @@
 /********* 전역선언 **********/
-
+var bannerInterval;
+var bannerIdx = 0;
 
 
 /********* 이벤트선언 **********/
 $(".header-wrapper .bar-wrap").click(onBarWrap);
+bannerInterval = setInterval(onBannerInterval, 4000);
+
 
 
 /********* 이벤트콜백 **********/
 function onBarWrap() {
 	$(".header-wrapper .mo-navi-wrapper").toggleClass('active');
+}
+function onBannerInterval(){
+	bannerIdx = bannerIdx == 2 ? 0 : bannerIdx + 1 ;
+	$(".main-wrapper .banner").hide();
+	$(".main-wrapper .banner").eq(bannerIdx).show();
 }
 
 
