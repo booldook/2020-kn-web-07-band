b6e85a794f3121a249012356f6b0312c52072c4d
booldook <booldook@gmail.com>
2020-12-31 11:22:45 +0900

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,13 @@
 
 
 /********* 이벤트선언 **********/
-
+$(".header-wrapper .bar-wrap").click(onBarWrap);
 
 
 /********* 이벤트콜백 **********/
-
+function onBarWrap() {
+	$(".header-wrapper .mo-navi-wrapper").toggleClass('active');
+}
 
 
 /********* 사용자함수 **********/
